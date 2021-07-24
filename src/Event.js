326aacded8e8deb941ef3e4981e049e07a66b519
DanielleP1086 <>
2021-07-24 20:58:29 +0200

    Add Number of Events Component and integrate final API

--- src/Event.js
+++ src/Event.js
@@ -2,7 +2,19 @@ import React, { Component } from "react";
 
 class Event extends Component {
   render() {
-    return <div></div>;
+    const {
+      summary,
+      location,
+      start,
+      htmlLink,
+      description,
+    } = this.props.event;
+
+    return (
+      <div>
+        <h2>{summary}</h2>
+      </div>
+    );
   }
 }
 export default Event;
